8ef2a2165231e4c6b468013a6d0190264647b843
nopi <novianto74@gmail.com>
2013-11-04 13:13:59 +0700

    4

--- protected/views/layouts/front.php
+++ protected/views/layouts/front.php
@@ -7,56 +7,49 @@
 		<meta name="description" content="A form is a collection of user input elements" />
 		<meta name="keywords" content="rekomendasi,tempat pkli, pkli, sistem informasi,magang" />
 		<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css">
+		<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css">
 		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-2.0.3.min.js"></script>
 		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<nav class="navbar navbar-default" role="navigation">
-		  <div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-			  <span class="sr-only">Toggle navigation</span>
-			  <span class="icon-bar"></span>
-			  <span class="icon-bar"></span>
-			  <span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#">SPK PKLI</a>
-		  </div>
-
-		  <!-- Collect the nav links, forms, and other content for toggling -->
-		  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav">
-			  <li class="active"><a href="#">Home</a></li>
-			  <li><a href="#">Pengumuman</a></li>
-			  <li><a href="#">About</a></li>
-			</ul>
-			
-			<ul class="nav navbar-nav navbar-right">
-				<li>
-					<form class="navbar-form navbar-left" role="search">
-						<div class="form-group"><input type="text" class="form-control" placeholder="Search"></div>
-						<button type="submit" class="btn btn-default">Submit</button>
-					</form>
-				</li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Masuk</a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Action</a></li>
-						<li><a href="#">Another action</a></li>
-						<li><a href="#">Something else here</a></li>
-						<li class="divider"></li>
-						<li><a href="#">Separated link</a></li>
+			<div class="container">	
+				<div class="navbar-header">
+			  	<!-- Collect the nav links, forms, and other content for toggling -->
+			  	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="/home">Home</a></li>
+						<li><a href="/pengumuman">Pengumuman</a></li>
+						<li><a href="/about">About</a></li>
 					</ul>
-				</li>
-			</ul>
-		  </div><!-- /.navbar-collapse -->
+				
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<form class="navbar-form navbar-left" role="search">
+								<div class="form-group"><input type="text" class="form-control" placeholder="Search"></div>
+								<button type="submit" class="btn btn-default">Submit</button>
+							</form>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Masuk</a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li class="divider"></li>
+								<li><a href="#">Separated link</a></li>
+							</ul>
+						</li>
+					</ul>
+			  	</div><!-- /.navbar-collapse -->
+		  	</div>
 		</nav>
 		<?php echo $content; ?>
-		<div class="ui inverted segment">
-	 		 <p class="ui left floated header"> Copyright &copy; <?php echo date('Y'); ?> SIC 2013 All Rights Reserved 
-	 		 </p>
-	 		 <p class="ui right floated header">
-	 		 	<?php echo Yii::powered(); ?>
-	 		 </p>
-		</div>
+		<footer>
+			<div class="container">
+		 		 <div class="pull-left"> Copyright &copy; <?php echo date('Y'); ?> SIC 2013 All Rights Reserved</div>
+		 		 <div class="pull-right"><?php echo Yii::powered(); ?></div>
+		 	</div>	 
+		</footer>
 	</body>
 </html>

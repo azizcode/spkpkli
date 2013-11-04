88fbab989597c0b5488f90be92c8f02024839cbd
H. Abdul Aziz <azizcode@gmail.com>
2013-11-05 04:51:39 +0700

    home

--- protected/views/front/index.php
+++ protected/views/front/index.php
@@ -4,19 +4,20 @@
 		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
 		<li data-target="#carousel-example-generic" data-slide-to="2"></li>
 	</ol>
-
-	<!-- Wrapper for slides -->
 	<div class="carousel-inner">
 		<div class="item active">
-			<img src="..." alt="...">
+			<?php echo CHtml::image(Yii::app()->request->baseUrl.'/gambar/1.jpg','1.jpg'); ?>
 			<div class="carousel-caption"></div>
 		</div>
 		<div class="item">
-			<img src="..." alt="...">
+			<?php echo CHtml::image(Yii::app()->request->baseUrl.'/gambar/2.jpg','2.jpg'); ?>
+			<div class="carousel-caption"></div>
+		</div>
+		<div class="item">
+			<?php echo CHtml::image(Yii::app()->request->baseUrl.'/gambar/3.jpg','3.jpg'); ?>
 			<div class="carousel-caption"></div>
 		</div>
 	</div>
-	
 	<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
 		<span class="glyphicon glyphicon-chevron-left"></span>
 	</a>

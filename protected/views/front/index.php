8f8dda9a3dc2cd63f2b5924e3aaaedda64180fb1
H. Abdul Aziz <azizcode@gmail.com>
2013-11-05 08:02:06 +0700

    gambar

--- protected/views/front/index.php
+++ protected/views/front/index.php
@@ -25,3 +25,33 @@
 		<span class="glyphicon glyphicon-chevron-right"></span>
 	</a>
 </div>
+<div class="row">
+  <div class="col-md-4 col-lg-4">Lorem ipsum dolor sit amet, quo dictas iracundia intellegat no, in sit petentium quaerendum. Per vide decore ex, at virtute consetetur consectetuer per. Ius ei tractatos dissentiet ullamcorper. Per id augue vocent periculis, ut est persius discere facilisis, tale autem omnes nam at.
+
+Ne nam nonumy noluisse ullamcorper, id mel viris ignota commune. Ad voluptaria accommodare eum. Ex impetus pericula sea, ad has munere sapientem consectetuer, mei ex quis nemore delenit. No cum vocibus posidonium, eum probo dicunt ex, cum ubique dictas id. Cum minim dolor complectitur te, elitr persecuti ne duo.
+
+Pri liber nominavi cu, appetere liberavisse complectitur ea sea. Id dicant conclusionemque vim, ad nihil legere adolescens qui, in homero doctus mea. Qui ad viderer vivendo principes, reque oportere te vim, vel an dolorem ancillae accumsan. Ex est feugait aliquando adversarium.
+
+Et pri etiam invidunt principes, delicata imperdiet vis no. An decore eirmod eos, ut vis solum postea offendit, quo ad duis primis philosophia. Reque aliquid accusamus nam ut. Meliore menandri at eos.
+
+Eos lorem probatus ea. Vim etiam inani cu, no vis veritus maluisset. Ad unum consectetuer his. No perpetua dignissim pri.</div>
+  <div class="col-md-4 col-lg-4">Lorem ipsum dolor sit amet, quo dictas iracundia intellegat no, in sit petentium quaerendum. Per vide decore ex, at virtute consetetur consectetuer per. Ius ei tractatos dissentiet ullamcorper. Per id augue vocent periculis, ut est persius discere facilisis, tale autem omnes nam at.
+
+Ne nam nonumy noluisse ullamcorper, id mel viris ignota commune. Ad voluptaria accommodare eum. Ex impetus pericula sea, ad has munere sapientem consectetuer, mei ex quis nemore delenit. No cum vocibus posidonium, eum probo dicunt ex, cum ubique dictas id. Cum minim dolor complectitur te, elitr persecuti ne duo.
+
+Pri liber nominavi cu, appetere liberavisse complectitur ea sea. Id dicant conclusionemque vim, ad nihil legere adolescens qui, in homero doctus mea. Qui ad viderer vivendo principes, reque oportere te vim, vel an dolorem ancillae accumsan. Ex est feugait aliquando adversarium.
+
+Et pri etiam invidunt principes, delicata imperdiet vis no. An decore eirmod eos, ut vis solum postea offendit, quo ad duis primis philosophia. Reque aliquid accusamus nam ut. Meliore menandri at eos.
+
+Eos lorem probatus ea. Vim etiam inani cu, no vis veritus maluisset. Ad unum consectetuer his. No perpetua dignissim pri.</div>
+  <div class="col-md-4 col-lg-4">Lorem ipsum dolor sit amet, quo dictas iracundia intellegat no, in sit petentium quaerendum. Per vide decore ex, at virtute consetetur consectetuer per. Ius ei tractatos dissentiet ullamcorper. Per id augue vocent periculis, ut est persius discere facilisis, tale autem omnes nam at.
+
+Ne nam nonumy noluisse ullamcorper, id mel viris ignota commune. Ad voluptaria accommodare eum. Ex impetus pericula sea, ad has munere sapientem consectetuer, mei ex quis nemore delenit. No cum vocibus posidonium, eum probo dicunt ex, cum ubique dictas id. Cum minim dolor complectitur te, elitr persecuti ne duo.
+
+Pri liber nominavi cu, appetere liberavisse complectitur ea sea. Id dicant conclusionemque vim, ad nihil legere adolescens qui, in homero doctus mea. Qui ad viderer vivendo principes, reque oportere te vim, vel an dolorem ancillae accumsan. Ex est feugait aliquando adversarium.
+
+Et pri etiam invidunt principes, delicata imperdiet vis no. An decore eirmod eos, ut vis solum postea offendit, quo ad duis primis philosophia. Reque aliquid accusamus nam ut. Meliore menandri at eos.
+
+Eos lorem probatus ea. Vim etiam inani cu, no vis veritus maluisset. Ad unum consectetuer his. No perpetua dignissim pri.</div>
+</div>
+

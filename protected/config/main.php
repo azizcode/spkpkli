c157f15f5903736b2d21c3b1e8ffdd9de5b1fae5
nopi <novianto74@gmail.com>
2013-11-05 22:41:35 +0700

    Semangat !!!

--- protected/config/main.php
+++ protected/config/main.php
@@ -44,6 +44,7 @@ return array(
 			'showScriptName'=>false,
 			'rules'=>array(
 				'daftar'	   => 'front/daftar',
+				'perusahaan'	   => 'perusahaan/index',
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',

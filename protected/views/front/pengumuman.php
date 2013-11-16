c6f7977db46f92a6b40ac3ddea10d9d12139c269
nopi <novianto74@gmail.com>
2013-11-16 17:18:35 +0700

    pengumuman

--- protected/views/front/pengumuman.php
+++ protected/views/front/pengumuman.php
@@ -3,25 +3,17 @@
         <div class="col-lg-8">
         
           <!-- the actual blog post: title/author/date/content -->
-          <h1>A Simple Blog Template for Bootstrap 3</h1>
-          <p class="lead">by <a href="index.html">Start Bootstrap</a></p>
+          <h1>Pengumuman Program PKLI</h1>
           <hr>
           <p><span class="glyphicon glyphicon-time"></span> Posted on August 24, 2013 at 9:00 PM</p>
           <hr>
-          <img src="http://placehold.it/900x300" class="img-responsive">
+          <img src="<?php echo Yii::app()->request->baseUrl.'/gambar/8.jpg'; ?>" class="img-responsive">
           <hr>
           <p class="lead">Science cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.</p>
           <p>You know, being a test pilot isn't always the healthiest business in the world.</p>
           <p>Cookie jelly beans soufflé icing. Gummi bears tootsie roll powder chupa chups cheesecake chocolate jelly-o lollipop lollipop. Halvah applicake chupa chups. Marshmallow chocolate jujubes icing lollipop gummi bears chupa chups pudding bonbon. Jelly beans jelly soufflé jujubes. Sesame snaps lollipop icing donut lemon drops soufflé.</p>
           <p>Donut caramels gingerbread. Sweet roll macaroon pastry cotton candy oat cake sesame snaps biscuit lemon drops dessert. Candy canes carrot cake danish carrot cake soufflé jelly chocolate cake muffin. Topping brownie donut. Oat cake marzipan dragée cheesecake. Donut chocolate cake jujubes tart dragée toffee.</p>
           <p>Tilefish electric knifefish salmon shark southern Dolly Varden. Pacific argentine tope golden shiner ilisha barreleye loosejaw catla, dogteeth tetra catfish tenpounder nase scup Ragfish brotula." Codlet brook lamprey pleco, Japanese eel convict cichlid titan triggerfish, plownose chimaera topminnow Black scalyfin. Walleye pollock, blue shark Sacramento blackfish prickleback airbreathing catfish yellowfin cutthroat trout, goby southern sandfish. North Pacific daggertooth dorab cepalin weever flying gurnard.</p>
-          <p><strong>Placeholder text by:</strong></p>
-          <ul>
-            <li><a href="http://spaceipsum.com/">Space Ipsum</a></li>
-            <li><a href="http://cupcakeipsum.com/">Cupcake Ipsum</a></li>
-            <li><a href="http://tunaipsum.com/">Tuna Ipsum</a></li>
-          </ul>
-
           <hr>
 
           <!-- the comment box -->
@@ -33,17 +25,7 @@
               </div>
               <button type="submit" class="btn btn-primary">Submit</button>
             </form>
-          </div>
-          
-          <hr>
-
-          <!-- the comments -->
-          <h3>Start Bootstrap <small>9:41 PM on August 24, 2013</small></h3>
-          <p>This has to be the worst blog post I have ever read. It simply makes no sense. You start off by talking about space or something, then you randomly start babbling about cupcakes, and you end off with random fish names.</p>
-
-          <h3>Start Bootstrap <small>9:47 PM on August 24, 2013</small></h3>
-          <p>Don't listen to this guy, any blog with the categories 'dinosaurs, spaceships, fried foods, wild animals, alien abductions, business casual, robots, and fireworks' has true potential.</p>
-
+          </div>      
         </div>
         
         <div class="col-lg-4">

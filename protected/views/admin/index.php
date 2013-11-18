c0c2e33685e9a037bc5597d16d589ae66d248674
zaezhu <zaenal.aidin94@gmail.com>
2013-11-18 12:59:07 +0700

    admin

---
+++ protected/views/admin/index.php
@@ -0,0 +1,85 @@
+<div id="page-wrapper">
+  <div class="row">
+    <div class="col-lg-12">
+      <ol class="breadcrumb">
+        <li class="active"><i class="fa fa-dashboard"></i> Dashboard</li>
+      </ol>
+      <h1><small>Sistem Pendukung Keputusan Bidang Keahlian PKLI</small></h1>
+      <div class="alert alert-success alert-dismissable"> 
+        Selamat datang <b></b>
+        <p>Di sistem informasi rekomendasi PKLI , sebagai instansi ada memiliki peran yang sanggat baik dalam proses pembelajaran mahasiswa. Instansi ini di harapkan mampu memberikan bekal atau pengalaman sebelum mahasiswa tejun ke dunia kerja yang sesungguhnya</p>
+        <b>Kami ucapakn terimakasih telah menggunakan sistem kami</b>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+      <div class="col-lg-6">
+        <div class="panel panel-success">
+          <div class="panel-heading">
+            <div class="row">
+              <div class="col-xs-12">
+                <div class="col-xs-2 text-center">
+                  <i class="fa fa-file-o fa-5x"></i>
+                  <p class="announcement-text">Daftar</p>
+                </div>
+                <div class="col-xs-2 text-center">
+                  <i class="fa fa-plus fa-3x"></i>
+                </div>
+                <div class="col-xs-2 text-center">
+                  <i class="fa fa-check-square-o fa-5x"></i>
+                  <p class="announcement-text">Kebutuhan</p>
+                </div>            
+                <div class="col-xs-3 text-center">
+                  <i class="fa fa-arrow-right fa-3x"></i>
+                </div>
+                <div class="col-xs-3 text-center">
+                  <i class="fa fa-list fa-5x"></i>
+                  <p class="announcement-text">Program</p>
+                </div>
+              </div>
+              <div class="col-xs-4 text-center">
+                <i class="fa fa-user fa-8x"></i>
+              </div>
+              <div class="col-xs-8">
+                <p class="text-bottom">Input Program PKLI</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="panel panel-success">
+          <div class="panel-heading">
+            <div class="row">
+              <div class="col-xs-12">
+                <div class="col-xs-2 text-center">
+                  <i class="fa fa-list fa-5x"></i>
+                  <p class="announcement-text">Program</p>
+                </div>
+                <div class="col-xs-3 text-right">
+                  <i class="fa fa-arrow-right fa-3x"></i>
+                </div>
+                <div class="col-xs-2 text-center">
+                  <i class="fa fa-upload fa-5x"></i>
+                  <p class="announcement-text">Mahasiswa</p>
+                </div>            
+                <div class="col-xs-2 text-center">
+                  <i class="fa fa-arrow-right fa-3x"></i>
+                </div>
+                <div class="col-xs-3 text-center">
+                  <i class="fa fa-heart fa-5x"></i>
+                  <p class="announcement-text">PKLI</p>
+                </div>
+              </div>
+              <div class="col-xs-4 text-center">
+                <i class="fa fa-user fa-8x"></i>
+              </div>
+              <div class="col-xs-8">
+                <p class="text-bottom">Lihat peserta</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+ </div>
---
+++ protected/views/layouts/admin.php
@@ -0,0 +1,97 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>SISTEM REKOMENDASI PKLI - <?php echo CHtml::encode($this->pageTitle); ?></title>
+		<meta name="description" content="A form is a collection of user input elements" />
+		<meta name="keywords" content="rekomendasi,tempat pkli, pkli, sistem informasi,magang" />
+		<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css">
+		<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/sb-admin.css">
+		<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css">
+		<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/font-awesome.min.css">
+		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-2.0.3.min.js"></script>
+		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap.min.js"></script>
+		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/moment.js"></script>
+		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/modal.js"></script>
+
+	</head>
+	<body>
+		<header class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="<?php echo Yii::app()->request->baseUrl; ?>">SPK PKLI</a>
+			</div>
+		  	<!-- Collect the nav links, forms, and other content for toggling -->
+		  	<nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+				<ul class="nav navbar-nav">
+					<li class="menu <?php if($this->action=='home'){ ?>odd<?php } ?>">
+						<a href="<?php echo Yii::app()->request->baseUrl; ?>">
+							<span class="glyphicon glyphicon-home"></span>
+							<span class="glyphicon-class">Home</span>
+						</a>
+					</li>
+					<li class="menu <?php if($this->action=='pengumuman'){ ?>odd<?php } ?>">
+						<a href="<?php echo Yii::app()->request->baseUrl; ?>/pengumuman">
+							<span class="glyphicon glyphicon-warning-sign"></span>
+							<span class="glyphicon-class">Pengumuman</span>
+						</a>
+					</li>
+					<li class="menu <?php if($this->action=='about'){ ?>odd<?php } ?>">
+						<a href="<?php echo Yii::app()->request->baseUrl; ?>/about">
+							<span class="glyphicon glyphicon-info-sign"></span>
+							<span class="glyphicon-class">About</span>
+						</a>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">	
+					<li class="menu pull-right" style="border:none;">
+						<a href="<?php echo Yii::app()->request->baseUrl; ?>/admin/profil">
+							<span class="glyphicon glyphicon-user"></span>
+							<span class="glyphicon-class">Admin</span>
+						</a>
+					</li>	
+				</ul>
+		  	</nav><!-- /.navbar-collapse -->
+		</header>
+		<div id="wrapper">
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav side-nav">
+				<li class="active sidebar"><a href="<?php echo Yii::app()->request->baseUrl; ?>/admin"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+				<li class="dropdown sidebar"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-table"></i>Laporan<b class="caret"></b></a>
+					<ul>
+                    	<li class="t"><a href="<?php echo Yii::app()->request->baseUrl; ?>/admin/rekomendasi">Mahasiswa</a></li>
+                        <li class="t"><a href="<?php echo Yii::app()->request->baseUrl; ?>/admin/rekomendasi">Instansi</a></li>
+                    </ul>
+				</li>
+				<li class="dropdown sidebar"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-table"></i>Kelola Sistem<b class="caret"></b></a>
+					<ul>
+                    	<li class="t"><a href="<?php echo Yii::app()->request->baseUrl; ?>/admin/datamahasiswa">Mahasiswa</a></li>
+                        <li class="t"><a href="<?php echo Yii::app()->request->baseUrl; ?>/admin/datainstansi">Instansi</a></li>
+						<li class="t"><a href="<?php echo Yii::app()->request->baseUrl; ?>/admin/pengumuman">Pengumuman</a></li>
+                    </ul>
+				</li>
+				<li class="sidebar"><a href="<?php echo Yii::app()->request->baseUrl; ?>/logout"><i class="fa fa-sign-out"></i>Keluar</a></li>
+				</ul>
+			</div>
+			<?php echo $content; ?>
+			<div class="footer">
+				 <div class="footer-left"><span> Copyright &copy; <?php echo date('Y'); ?> SIC 2013 All Rights Reserved</span></div>
+				 <div class="footer-right"><span><?php echo Yii::powered(); ?></span></div>
+			</div>
+		</div>
+	  	<script>
+	  		$(function() {
+			  $('.dropdown-toggle').dropdown();
+			  $('.dropdown input, .dropdown label').click(function(e) {
+			    e.stopPropagation();
+			  });
+			});
+	  	</script>
+	</body>
+</html>
